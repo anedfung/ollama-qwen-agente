8d45380cf3416310f83df501bcc041f5df20c4de
Andres <anedfung@gmail.com>
2026-05-07 20:30:59 -0600

    Prueba de publicación

--- main.py
+++ main.py
@@ -3,9 +3,6 @@ from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 from agent.agent import preguntar_agente
 
-import sys
-print("Python version:", sys.version)
-
 app = FastAPI()
 
 # Define allowed origins
@@ -30,3 +27,7 @@ class Query(BaseModel):
 def chat(query: Query):
     respuesta = preguntar_agente(query.question)
     return {"response": respuesta}
+
+@app.get("/")
+def root():
+    return {"message": "API funcionando en Render 🚀"}
