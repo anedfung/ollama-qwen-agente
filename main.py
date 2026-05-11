71b20c4fc1245e877613a9765f5b64dd8d2a44e3
Andres <anedfung@gmail.com>
2026-05-10 18:03:16 -0600

    prueba simple

--- main.py
+++ main.py
@@ -1,33 +1,7 @@
 from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
-from pydantic import BaseModel
-from agent.agent import preguntar_agente
 
 app = FastAPI()
 
-# Define allowed origins
-origins = [
-"http://localhost",
-"http://localhost:3000",
-"https://website-1-lovat.vercel.app/"
-]
-
-app.add_middleware(
-CORSMiddleware,
-allow_origins=origins, # List of allowed origins
-allow_credentials=True,
-allow_methods=["*"], # Allow all HTTP methods
-allow_headers=["*"], # Allow all headers
-)
-
-class Query(BaseModel):
-    question: str
-
-@app.post("/chat")
-def chat(query: Query):
-    respuesta = preguntar_agente(query.question)
-    return {"response": respuesta}
-
 @app.get("/")
 def root():
-    return {"message": "API funcionando en Render 🚀"}
+    return {"message": "Hola Render 🚀"}
